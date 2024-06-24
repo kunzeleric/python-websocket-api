d31437716e056d1f18760910a18dadfc0d518a3c
Eric Kunzel <kunzel.eric@gmail.com>
2024-06-23 21:18:19 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+from repository.database import db
+from db_models.payment import Payment
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///payments.db"
+app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
+
+db.init_app(app)
+
+@app.route("/payments/pix", methods=["POST"])
+def create_payment_pix():
+    data = request.get_json()
+    if "value" not in data:
+        return jsonify({"error": "Missing 'value' field."}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data["value"], expiration_date=expiration_date)
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify(
+        {"message": "The payment has been created.", "payment": new_payment.to_dict()}
+    )
+
+
+@app.route("/payments/pix/confirmation", methods=["POST`"])
+def pix_confirmation():
+    return jsonify({"message": "The payment has been confirmed."})
+
+
+@app.route("/payments/pix/<int:payment_id>", methods=["GET"])
+def payment_pix_page(payment_id):
+    return "pagamento pix"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db_models/payment.py
@@ -0,0 +1,19 @@
+from repository.database import db
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float)
+    paid = db.Column(db.Boolean,default=False)
+    bank_payment_id = db.Column(db.Integer, nullable=True)
+    qr_code = db.Column(db.String(100), nullable=True)
+    expiration_date = db.Column(db.DateTime)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date.strftime("%Y-%m-%d %H:%M:%S")
+        }
