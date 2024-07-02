92a2785838b36cbbb02e0edb7a3a2de084cc0984
Eric Kunzel <kunzel.eric@gmail.com>
2024-07-02 17:50:36 -0300

    feat: created test file for pix class and added pytest to requirements

--- payments/pix.py
+++ payments/pix.py
@@ -6,14 +6,14 @@ class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # criar o pagamento na instituicao financeira
         bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f"hash_payment_{bank_payment_id}"
 
         img = qrcode.make(hash_payment)
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {
             "bank_payment_id": bank_payment_id,
---
+++ tests/test_pix.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.append("../")
+
+import pytest
+import os
+from payments.pix import Pix
+
+
+def test_pix_create_payment():
+    pix_instance = Pix()
+
+    payment_info = pix_instance.create_payment(base_dir="../")
+
+    assert "bank_payment_id" in payment_info
+    assert "qr_code_path" in payment_info
+
+    os.path.isfile(f"/static/img/{payment_info['qr_code_path']}.png")
